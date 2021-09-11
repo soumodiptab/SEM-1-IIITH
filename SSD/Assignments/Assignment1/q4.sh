59751b50c2c62cdaea0d6c00168d283a5662af33
soumodiptab <sbose2019@gmail.com>
2021-09-11 22:44:43 +0530

    question 4 of SSD done

---
+++ SSD/Assignments/Assignment1/q4.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+function int_to_rom()
+{
+	local numeral=""
+	local num=$1
+	local rom=(M CM D CD C XC L XL X IX V IV I)
+    	local dec=(1000 900 500 400 100 90 50 40 10 9 5 4 1)
+    	local i=0
+    	while [ $num -gt 0  ]
+    	do
+    		local val=${dec[$i]}
+    		local r=${rom[$i]}
+    		local div=$(( num / val ))
+    		num=$(( num % val ))
+    		while [ $div -gt 0 ]
+    		do
+    			numeral=$numeral$r
+    			((div--))
+    		done
+    		((i++))
+    	done
+    	echo $numeral
+}
+function rom_to_int()
+{
+	local number=$(
+    echo $1 |
+    sed 's/CM/DCD/g' |
+    sed 's/M/DD/g' |
+    sed 's/CD/CCCC/g' |
+    sed 's/D/CCCCC/g' |
+    sed 's/XC/LXL/g' |
+    sed 's/C/LL/g' |
+    sed 's/XL/XXXX/g' |
+    sed 's/L/XXXXX/g' |
+    sed 's/IX/VIV/g' |
+    sed 's/X/VV/g' |
+    sed 's/IV/IIII/g' |
+    sed 's/V/IIIII/g' |
+    tr -d '\n' |
+    wc -m)
+	echo $number
+}
+if [[ $# == 1 ]]
+then
+	numeral=$(int_to_rom $1)
+	echo $numeral
+elif [[ $# == 2 ]]
+then
+	x=$1
+	y=$2
+	if [[ "${x//[IVXLCDM]/}" == "" && "${y//[IVXLCDM]/}" == "" ]]
+	then
+		num1=$(rom_to_int $x)
+		num2=$(rom_to_int $y)
+		sum=$(( num1 + num2 ))
+		echo $sum
+    elif [[ "${x//[IVXLCDM]/}" == "" || "${y//[IVXLCDM]/}" == "" ]]
+    then
+        exit 
+    else
+        sum=$(( x + y ))
+        numeral=$(int_to_rom $sum)
+		echo $numeral
+    fi
+else
+	exit
+fi
+
+#rom_to_int $1
+
---
+++ SSD/Assignments/Assignment1/readme.md
@@ -0,0 +1,22 @@
+# SSD ASSIGNMENT 1
+## Question 1
+
+## Question 2
+
+## Question 3
+
+## Question 4
+#### Assumptions :
+Roman Numbers are in capital
+#### Utility Functions :
+##### `rom_to_int`:
+__Converts Roman Number to Integer__
+
+It extracts each substraction notation and replaces it with its corresponding lower unit roman numerals using the replace functionality of `sed` command.
+Then it removes a newline before doing a count using `tr` and `wc` commands.
+##### `int_to_rom`:
+__Converts Integer to Roman__
+
+Steps:
+1. If number 
+## Question 5
